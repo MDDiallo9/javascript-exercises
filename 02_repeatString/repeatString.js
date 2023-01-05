85668195de850aa67940044ef3f16c51ebdd994a
Mohamed <114567817+MDDiallo9@users.noreply.github.com>
2023-01-05 15:41:10 +0100

    exo2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string,num) {
+    tries = 0
+    if (num < 0){
+        return('ERROR')
+     }
+    phrase = ""
+    while (tries < num) {
+        tries++
+    phrase = phrase + string
+     
+    }
+    return (phrase)
 };
 
 // Do not edit below this line
