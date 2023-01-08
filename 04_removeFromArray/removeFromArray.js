65dc0637fc4958b4278d4846a6daf030e12140e6
Mohamed <114567817+MDDiallo9@users.noreply.github.com>
2023-01-08 16:47:59 +0100

    first 2 steps working

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,10 @@
-const removeFromArray = function() {
-
-};
-
+const removeFromArray = function(a,b,d) {
+        c = a.splice(b-1,1)
+        if (d != undefined){
+        e = a.splice(d-1,1)
+        }
+        
+        return a
+      };
 // Do not edit below this line
 module.exports = removeFromArray;
