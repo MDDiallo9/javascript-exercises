a985632f12755d9e51bb3f9dccd458f70553a9dc
Mohamed <114567817+MDDiallo9@users.noreply.github.com>
2023-01-10 21:04:24 +0100

    done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,11 +1,6 @@
-const removeFromArray = function(a,b,d) {
-    if (b < a.length && b > -1){
-        c = a.splice(b-1,1)
-      } 
-    if (d != undefined && d < a.length && d > -1){
-        e = a.splice(d-1,1)
-        }
-    return a
-      };
+const removeFromArray = function(a,b,d,e,f) {
+  let result = a.filter(a => a!== b && a !== d && a !== e && a !== f)
+  return result
+};
 // Do not edit below this line
 module.exports = removeFromArray;
