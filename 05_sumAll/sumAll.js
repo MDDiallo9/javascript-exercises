e00468b19845ac2be68f3d621925f066442f7c06
Mohamed <114567817+MDDiallo9@users.noreply.github.com>
2023-01-15 15:17:22 +0100

    6/6

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,10 +1,18 @@
 const sumAll = function(min,max) {
-    let sum = 0
- for (let i = min; i < max + 1 ; i++) {
+let sum = 0
+if (max > min && min > 0 && max > 0 && Number.isInteger(min) && Number.isInteger(max)){
+for (let i = min; i < max + 1 ; i++) {
     sum += i
  }
  return sum
-
+} else if (min > max && min > 0 && max > 0 && Number.isInteger(min) && Number.isInteger(max)){
+    for (let i = max; i < min + 1 ; i++) {
+        sum += i
+     }
+     return sum
+} else {
+    return "ERROR"
+}
 };
 
 // Do not edit below this line
