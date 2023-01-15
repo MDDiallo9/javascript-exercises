0b7baecc3399fc5fbfae891fab31848099fc8f06
Mohamed <114567817+MDDiallo9@users.noreply.github.com>
2023-01-15 15:26:54 +0100

    6/6

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
-
+// // Create a function that determines whether or not a given year is a leap year. Leap years are determined by the following rules:
+// leap year = divisible by 4 , indivisible par 100 à moins qu'elle soit divisible par 400
+// // > Leap years are years divisible by four (like 1984 and 2004). However, years divisible by 100 are not leap years (such as 1800 and 1900) 
+// unless they are divisible by 400 (like 1600 and 2000, which were in fact leap years). (Yes, it's all pretty confusing)
+const leapYears = function(year) {
+    if ((year % 4 == 0) && (year % 100 !== 0 || year % 400 == 0)){
+        return true
+    } else {
+        return false
+    }
 };
 
 // Do not edit below this line
